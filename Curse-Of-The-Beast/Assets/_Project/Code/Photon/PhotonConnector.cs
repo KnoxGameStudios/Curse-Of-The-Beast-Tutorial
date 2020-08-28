ee21d20b57f0f46d98cee3bba7325bc62f89b2e4
Corey Cooley <corey.a.cooley@gmail.com>
2020-08-27 20:41:13 -0400

    Photon Setup
    Part 1 of Photon and Playfab Unity series.

---
+++ Curse-Of-The-Beast/Assets/_Project/Code/Photon/PhotonConnector.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System;
+using Photon.Pun;
+using Photon.Realtime;
+
+public class PhotonConnector : MonoBehaviourPunCallbacks
+{
+    #region Unity Method
+    private void Start()
+    {
+        string randomName = $"Tester{Guid.NewGuid().ToString()}";
+        ConnectToPhoton(randomName);
+    }
+    #endregion
+    #region Private Methods
+    private void ConnectToPhoton(string nickName)
+    {
+        Debug.Log($"Connect to Photon as {nickName}");
+        PhotonNetwork.AuthValues = new AuthenticationValues(nickName);
+        PhotonNetwork.AutomaticallySyncScene = true;
+        PhotonNetwork.NickName = nickName;
+        PhotonNetwork.ConnectUsingSettings();
+    }
+    private void CreatePhotonRoom(string roomName)
+    {
+        RoomOptions ro = new RoomOptions();
+        ro.IsOpen = true;
+        ro.IsVisible = true;
+        ro.MaxPlayers = 4;
+        PhotonNetwork.JoinOrCreateRoom(roomName, ro, TypedLobby.Default);
+    }
+    #endregion
+    #region Public Methods
+    #endregion
+    #region Photon Callbacks
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("You have connected to the Photon Master Server");
+        if (!PhotonNetwork.InLobby)
+        {
+            PhotonNetwork.JoinLobby();
+        }
+    }
+    public override void OnJoinedLobby()
+    {
+        Debug.Log("You have connected to a Photon Lobby");
+        CreatePhotonRoom("TestRoom");
+    }
+    public override void OnCreatedRoom()
+    {
+        Debug.Log($"You have created a Photon Room named {PhotonNetwork.CurrentRoom.Name}");
+
+    }
+    public override void OnJoinedRoom()
+    {
+        Debug.Log($"You have joined the Photon room {PhotonNetwork.CurrentRoom.Name}");
+    }
+    public override void OnLeftRoom()
+    {
+        Debug.Log("You have left a Photon Room");
+    }
+    public override void OnJoinRoomFailed(short returnCode, string message)
+    {
+        Debug.Log($"You failed to join a Photon room: {message}");
+    }
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        Debug.Log($"Another player has joined the room {newPlayer.UserId}");
+    }
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        Debug.Log($"Player has left the room {otherPlayer.UserId}");
+    }
+    public override void OnMasterClientSwitched(Player newMasterClient)
+    {
+        Debug.Log($"New Master Client is {newMasterClient.UserId}");
+    }
+    #endregion
+}
